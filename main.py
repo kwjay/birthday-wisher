6234c744984267e1cd9b45856868fc44dc4eb5e5
kwjay <kwjay1002@gmail.com>
2023-11-22 22:37:47 +0100

    replaced wrong files

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import smtplib
+##################### Extra Hard Starting Project ######################
 
-sender_email = "email@gmail.com"
-sender_password = "password"
+# 1. Update the birthdays.csv
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=sender_email, password=sender_password)
-connection.sendmail(
-    from_addr=sender_email,
-    to_addrs="birthday_guy@gmail.com",
-    msg="Subject:Hello\n\nHello"
-)
-connection.close()
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+
+
