4202765370532b63f331a5f983d428a5a8cb4d04
kwjay <kwjay1002@gmail.com>
2023-11-23 14:21:20 +0100

    prepared strings to pack into mail

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-##################### Extra Hard Starting Project ######################
-# 1. Update the birthdays.csv
+import random
+import pandas
+import datetime as dt
 
-# 2. Check if today matches a birthday in the birthdays.csv
+LETTERS_PATHS = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
+SENDER_NAME = "Jakub"
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+def format_letter(celebrant_name):
+    letter_file = random.choice(LETTERS_PATHS)
+    with open(f"letter_templates/{letter_file}") as file:
+        letter = file.read()
+    letter.replace("[NAME]", celebrant_name)
+    letter.replace("[SENDER]", SENDER_NAME)
+    return letter
+
+
+def send_birthday_wishes(celebrant):
+    pass
+
+
+data_file = pandas.read_csv("birthdays.csv")
+data = data_file.to_dict(orient="records")
+today = dt.datetime.now()
+
+for person in data:
+    if person["month"] == today.month and person["day"] == today.day:
+        send_birthday_wishes(celebrant=person)
 
 # 4. Send the letter generated in step 3 to that person's email address.
 
