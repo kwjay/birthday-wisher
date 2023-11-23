38abfc637b912f8c465c21d856356315dc596123
kwjay <kwjay1002@gmail.com>
2023-11-23 14:31:43 +0100

    fixed issue with replace method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import pandas
 import datetime as dt
+import smtplib
 
 LETTERS_PATHS = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
 SENDER_NAME = "Jakub"
@@ -12,13 +13,13 @@ def format_letter(celebrant_name):
     letter_file = random.choice(LETTERS_PATHS)
     with open(f"letter_templates/{letter_file}") as file:
         letter = file.read()
-    letter.replace("[NAME]", celebrant_name)
-    letter.replace("[SENDER]", SENDER_NAME)
+    letter = letter.replace("[NAME]", celebrant_name)
+    letter = letter.replace("[SENDER]", SENDER_NAME)
     return letter
 
 
 def send_birthday_wishes(celebrant):
-    pass
+    print(format_letter(celebrant_name=celebrant["name"]))
 
 
 data_file = pandas.read_csv("birthdays.csv")
